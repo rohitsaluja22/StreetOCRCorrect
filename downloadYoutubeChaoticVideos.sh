a909f253aa5ef312ffdeb6efc02edb0d1c33fee3
rohitsaluja22 <rohitsaluja22@users.noreply.github.com>
2019-06-25 23:50:40 +0530

    Update downloadYoutubeChaoticVideos.sh

--- downloadYoutubeChaoticVideos.sh
+++ downloadYoutubeChaoticVideos.sh
@@ -1,4 +1,4 @@
-#High resolution videos (youtube-dl not working, download from keepvid.pro download highest quality videos without sound)
+#High resolution videos:-
 https://www.youtube.com/watch?v=DBKHzo2u6Z4
 https://www.youtube.com/watch?v=V3r_aqNy3a0
 https://www.youtube.com/watch?v=uenBM7OV3oY
@@ -6,26 +6,26 @@ https://www.youtube.com/watch?v=L4sZ0vBd9Gw
 https://www.youtube.com/watch?v=77MpjKvOgz0
 
 
-#Low resolution videos
-youtube-dl https://www.youtube.com/watch?v=9n8aFscUYz8
-youtube-dl https://www.youtube.com/watch?v=HjBAyA30G6M
-youtube-dl https://www.youtube.com/watch?v=YxYcq9xKegI
-youtube-dl https://www.youtube.com/watch?v=Fn-kszLsams
-youtube-dl https://www.youtube.com/watch?v=q4LkY9y0_20
-youtube-dl https://www.youtube.com/watch?v=A-8x87ZEh6w
-youtube-dl https://www.youtube.com/watch?v=BWKR0dLpc78
-youtube-dl https://www.youtube.com/watch?v=mWjOTKpS9II
-youtube-dl https://www.youtube.com/watch?v=YAJI0nJkLH4
-youtube-dl https://www.youtube.com/watch?v=edLo5zdTstk
-youtube-dl https://www.youtube.com/watch?v=HuiTHc1sMHI
-youtube-dl https://www.youtube.com/watch?v=tlxCIg684IE
-youtube-dl https://www.youtube.com/watch?v=dy3Vy1dFAWU
-youtube-dl https://www.youtube.com/watch?v=nSazjROaYkQ
-youtube-dl https://www.youtube.com/watch?v=RutJoViMPtI
-youtube-dl https://www.youtube.com/watch?v=CJoZA-bMBU0
-youtube-dl https://www.youtube.com/watch?v=DM6mJIhg2gA
-youtube-dl https://www.youtube.com/watch?v=D7rwaZnS7zg
-youtube-dl https://www.youtube.com/watch?v=tkbgzXawqWQ
-youtube-dl https://www.youtube.com/watch?v=kQXRIsNRy1A
-youtube-dl https://www.youtube.com/watch?v=7bkszZoHj20
-youtube-dl https://www.youtube.com/watch?v=DEeR15XCY0g&feature=youtu.be
+#Low resolution videos:-
+https://www.youtube.com/watch?v=9n8aFscUYz8
+https://www.youtube.com/watch?v=HjBAyA30G6M
+https://www.youtube.com/watch?v=YxYcq9xKegI
+https://www.youtube.com/watch?v=Fn-kszLsams
+https://www.youtube.com/watch?v=q4LkY9y0_20
+https://www.youtube.com/watch?v=A-8x87ZEh6w
+https://www.youtube.com/watch?v=BWKR0dLpc78
+https://www.youtube.com/watch?v=mWjOTKpS9II
+https://www.youtube.com/watch?v=YAJI0nJkLH4
+https://www.youtube.com/watch?v=edLo5zdTstk
+https://www.youtube.com/watch?v=HuiTHc1sMHI
+https://www.youtube.com/watch?v=tlxCIg684IE
+https://www.youtube.com/watch?v=dy3Vy1dFAWU
+https://www.youtube.com/watch?v=nSazjROaYkQ
+https://www.youtube.com/watch?v=RutJoViMPtI
+https://www.youtube.com/watch?v=CJoZA-bMBU0
+https://www.youtube.com/watch?v=DM6mJIhg2gA
+https://www.youtube.com/watch?v=D7rwaZnS7zg
+https://www.youtube.com/watch?v=tkbgzXawqWQ
+https://www.youtube.com/watch?v=kQXRIsNRy1A
+https://www.youtube.com/watch?v=7bkszZoHj20
+https://www.youtube.com/watch?v=DEeR15XCY0g&feature=youtu.be
